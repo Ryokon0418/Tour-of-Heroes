3314a846f345a2b226dfe4732e9f76417ee6b568
Ryokon0418 <ryoko.nishikawa418@gmail.com>
2021-02-27 17:53:51 +0900

    [fix] Set routes from and to each apps.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService} from './message.service';
 
 @Injectable({
@@ -13,7 +15,13 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes.')
-    return heroes;
+    return of(HEROES);
+  }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    // @ts-ignore
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
 
