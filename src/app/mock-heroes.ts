99adf46aa87c0820c3df78384533ea615efc0b86
Ryokon0418 <ryoko.nishikawa418@gmail.com>
2021-02-27 12:48:18 +0900

    [Add] Heroes list.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Roland'},
+  { id: 2, name: 'Bellara'},
+  { id: 3, name: 'Roger'},
+  { id: 4, name: 'Taneda'},
+  { id: 5, name: 'Kharannah'},
+  { id: 6, name: 'Azariel'},
+  { id: 7, name: 'Lucky'},
+  { id: 8, name: 'Silver Moon'},
+  { id: 9, name: 'Sharazar'},
+  { id: 10, name: 'Rok'}
+];
+
