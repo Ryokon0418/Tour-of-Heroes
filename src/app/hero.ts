775647e2200b5b805b4dc309a14d3d5e6237cd58
Ryokon0418 <ryoko.nishikawa418@gmail.com>
2021-02-27 11:53:23 +0900

    [Create] Add Hero app.

---
+++ src/app/hero.ts
@@ -0,0 +1,5 @@
+// Heroのクラスを作成するファイル
+export interface Hero {
+  id: number;
+  name: string;
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
+  // CSSが適用される対象を指定
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
+  // 適用するCSSファイルを指定
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
   constructor() { }
 
   ngOnInit(): void {
